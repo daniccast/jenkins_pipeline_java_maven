a84a0fb03519ecc2b1cfbe7cb5a3a2d16cdf7288
Daniela Cortés <39687467+daniccast@users.noreply.github.com>
2023-03-16 19:52:00 -0600

    Update Error.java

--- src/main/java/com/xyz/Error.java
+++ src/main/java/com/xyz/Error.java
@@ -1,29 +1,36 @@
+
+package com.xyz;
 import org.h2.security.SHA256;
 
 
-void getPassword(){
-  String inputString = "s3cr37";
-  byte[] key         = inputString.getBytes();
-
-  SHA256.getHMAC(key, message);
-}
-
-
-int foo(int a) {
-  int b = 12;
-  if (a == 1) {
-    return b;
+public class HelloJenkins {
+   public void getPassword(){
+    String inputString = "s3cr37";
+    byte[] key         = inputString.getBytes();
+    SHA256.getHMAC(key, message);
   }
-  return b; 
+
+  public int foo(int a) {
+    int b = 12;
+    if (a == 1) {
+      return b;
+    }
+    return b; 
+  }
+
+  public int fail(int a) {
+    int b = 12;
+    if (a == 1) {
+      return b;
+    } if (b==1){
+      return a;
+    }
+    return b;  
+  }
+	public String go(){
+		return "HelloJenkinsfile";
+	}
+
 }
 
-int fail(int a) {
-  int b = 12;
-  if (a == 1) {
-    return b;
-  } if (b==1){
-    return a;
-  }
-  return b;  
-}
 
